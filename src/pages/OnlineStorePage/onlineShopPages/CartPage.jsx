b1e255b3f03692c05f15e50dffa3a4d85c444987
Chen Maimon <chenmay1696@gmail.com>
2023-05-29 20:26:40 +0300

    finished checkout basic page without checkout redirect and continue shopping redirect

--- src/pages/OnlineStorePage/onlineShopPages/CartPage.jsx
+++ src/pages/OnlineStorePage/onlineShopPages/CartPage.jsx
@@ -2,10 +2,14 @@ import React, { useContext } from 'react'
 import { PRODUCTS } from '../products'
 import { ShopContext } from './Context/ShopContext'
 import { CartItem } from './Componenets/cartItem'
-
+import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
-  const {cartItems} = useContext(ShopContext);
+  const {cartItems, getTotalCartAmount} = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  
+  const navigate = useNavigate();
+
   return (
     <div className='cart'>
       <div>
@@ -18,6 +22,14 @@ const CartPage = () => {
           }
         })}
       </div>
+      {totalAmount > 0 ?
+        <div className='checkout'>
+          <p>Subtotal:   ₪{totalAmount}</p>
+          <button onClick={() => navigate('/onlineShop')}>Continue Shopping</button>
+          <button onClick={() => navigate('/onlineShop/cart/paymentPage')}>Checkout </button>
+        </div>
+        : <h1> Your Cart is Empty</h1>}
+
     </div>
   )
 }
