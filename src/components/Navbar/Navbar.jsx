c5c7956a6f2d25d29c92f4a13d0950cb82185fe4
Chen Maimon <chenmay1696@gmail.com>
2023-06-03 16:51:14 +0300

    finnal fixes before submmit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,15 +6,15 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const { loggedInUser } = useContext(UserContext);
-  console.log("loggedInUser",loggedInUser);
+  console.log("loggedInUser", loggedInUser);
   return (
     <div className="navbar">
       <div className="leftSide">
         <Type />
       </div>
-      {loggedInUser&&(
-      <div className="rightSide">
-         {loggedInUser.role === "ADMIN" ? (
+      {(loggedInUser && (
+        <div className="rightSide">
+          {loggedInUser.role === "ADMIN" ? (
             <Link to="/"> Home </Link>
           ) : (
             <>
@@ -25,8 +25,38 @@ const Navbar = () => {
               <Link to="/chat"> Q&A </Link>
             </>
           )}
-      </div>
-)}
+        </div>
+      )) ||
+        (loggedInUser && (
+          <div className="rightSide">
+            {loggedInUser.role !== "SUPERAPP_USER" ? (
+              <Link to="/"> Home </Link>
+            ) : (
+              <>
+                <Link to="/"> Home </Link>
+                <Link to="/shop"> Shop </Link>
+                <Link to="/onlineShop">Online Store</Link>
+                <Link to="/parks"> Parks </Link>
+                <Link to="/chat"> Q&A </Link>
+              </>
+            )}
+          </div>
+        )) ||
+        (loggedInUser && (
+          <div className="rightSide">
+            {loggedInUser.role !== "MINIAPP_USER" ? (
+              <Link to="/"> Home </Link>
+            ) : (
+              <>
+                <Link to="/"> Home </Link>
+                <Link to="/shop"> Shop </Link>
+                <Link to="/onlineShop">Online Store</Link>
+                <Link to="/parks"> Parks </Link>
+                <Link to="/chat"> Q&A </Link>
+              </>
+            )}
+          </div>
+        ))}
     </div>
   );
 };
